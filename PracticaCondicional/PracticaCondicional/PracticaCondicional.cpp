d08af48be7b0a9c180a188a1cea8d1d2ed1195cd
EmiliaGuz <64081262+EmiliaGuz@users.noreply.github.com>
2020-04-21 13:40:39 -0500

    Add files via upload

---
+++ PracticaCondicional/PracticaCondicional/PracticaCondicional.cpp
@@ -0,0 +1,43 @@
+// PracticaCondicional.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+/*Escribe un programa que pregunte al usuario 2 numeros
+obten los dos numeros y imprime suma, producto,diferencia y cociente*/
+{
+    int x;
+    int y;
+
+    cout << "Ingrese un numero: ";
+    cin >> x;
+    cout << "Ingrese un segundo numero: ";
+    cin >> y;
+
+    int suma = x + y;
+
+    int producto = x * y;
+
+    int diferencia = x - y;
+
+    int cociente = x / y;
+
+    cout << "La suma es: " << suma;
+    cout << "El producto es: " << producto;
+    cout << "La diferencia es: " << diferencia;
+    cout << "El cociente es: " << cociente;
+
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
