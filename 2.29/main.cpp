01aebb2a910a06aa3f30b4a6c088608c286e13d5
EmiliaGuz <64081262+EmiliaGuz@users.noreply.github.com>
2020-04-22 00:17:11 -0500

    Add files via upload

---
+++ 2.29/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int length=0;
+
+    int area = length * length *length;
+
+    int volumen = 6* length *length ;
+
+    cout << "Cara " <<length << endl;
+    cout << "Area " << area<< endl;
+    cout << "Volumen "<< volumen<< endl;
+    cout << ".........."<< endl;
+
+    length+=1;
+
+    int area1 = length * length *length;
+
+    int volumen1 = 6* length *length ;
+
+    cout << "Cara " <<length << endl;
+    cout << "Area " << area1<< endl;
+    cout << "Volumen "<< volumen1<< endl;
+    cout << ".........."<< endl;
+
+    length+=1;
+
+    int area2 = length * length *length;
+
+    int volumen2 = 6* length *length ;
+
+    cout << "Cara " <<length << endl;
+    cout << "Area " << area2<< endl;
+    cout << "Volumen "<< volumen2<< endl;
+    cout << ".........."<< endl;
+
+    length+=1;
+
+    int area3 = length * length *length;
+
+    int volumen3 = 6* length *length ;
+
+    cout << "Cara " <<length << endl;
+    cout << "Area " << area3<< endl;
+    cout << "Volumen "<< volumen3<< endl;
+    cout << ".........."<< endl;
+
+    length+=1;
+
+    int area4 = length * length *length;
+
+    int volumen4 = 6* length *length ;
+
+    cout << "Cara " <<length << endl;
+    cout << "Area " << area4<< endl;
+    cout << "Volumen "<< volumen4<< endl;
+    cout << ".........."<< endl;
+
+    return 0;
+}
